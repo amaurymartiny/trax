bd53ace983b0b342989a207f951ebb743471638b
Amaury Martiny <amaury.martiny@gmail.com>
2015-11-12 14:10:50 +0100

    Initialize dataset

--- main.py
+++ main.py
@@ -15,11 +15,20 @@ def read_csv(csv_file):
   with open(csv_file, 'rb') as f:
     reader = csv.reader(f, delimiter=',')
     user_data = list(reader)
+    return user_data
 
-    print user_data
-
-def neural_network(input):
+def neural_network(input_data):
   print "Neural Network"
+  dataset = ClassificationDataSet(9, 1, nb_classes=5) # 9 is the dimension of the input, 1 i don't know
+  for row in input_data:
+    # add all 9 features inside our input tuple
+    input_tuple = ()
+    for i in range(9):
+      input_tuple += (float(row[i+4]),)
+    # print float(row[3])
+    dataset.addSample(input_tuple, [int(row[3])])
+  for inpt, target in dataset:
+    print inpt, target
 
 # normalization function
 # x is data, minX is minimum values in each column and maxX is maximum values in each column
